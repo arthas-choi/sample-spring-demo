169c7d36abbd53955e7b1f12cb212ec00323a4e3
lemy0715 <lemy0715@gmail.com>
2020-09-22 19:57:43 +0900

    Update HomeController.java

--- src/main/java/com/nalbam/sample/demo/controller/HomeController.java
+++ src/main/java/com/nalbam/sample/demo/controller/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
         sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
 
         // host
-	    host += "</p> my sample commit test </p> Infinity Stone 0.3 DEMO -Test-001";
+	    host += "Infinity Stone 0.3 DEMO -Test-001";
         model.put("host", host);
 
         // date
