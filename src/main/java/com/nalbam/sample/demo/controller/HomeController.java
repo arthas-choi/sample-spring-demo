4220f5f14fc0cb305b3cee57fb4391a2376f05b2
jaehwan.choi <jaehwan.choi@bespinglobal.com>
2020-12-24 01:15:04 +0900

    Bugfix

--- src/main/java/com/nalbam/sample/demo/controller/HomeController.java
+++ src/main/java/com/nalbam/sample/demo/controller/HomeController.java
@@ -1,49 +1,50 @@
 package com.nalbam.sample.demo.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.TimeZone;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @Autowired
-    BuildProperties buildProperties;
-	
-    @GetMapping("/")
-    public String index(Map<String, Object> model) {
-        String host;
-        try {
-            host = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            host = "Unknown";
-            e.printStackTrace();
-        }
 
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+  @Autowired
+  BuildProperties buildProperties;
 
-	    // Artifact's name from the pom.xml file
-	    
-	modle.put("build_name", buildProperties.getName());
-	modle.put("build_version", buildProperties.getVersion());
-	modle.put("build_time", buildProperties.getTime());
-	modle.put("build_name", buildProperties.getArtifact());
-	 modle.put("build_name", buildProperties.getArtifact());
-    
-        // host
-	host += "<br>DevOps Application</br>";
-        model.put("host", host);
-        // date
-        model.put("date", sdf.format(new Date()));
-
-        return "index";
+  @GetMapping("/")
+  public String index(Map<String, Object> model) {
+    String host;
+    try {
+      host = InetAddress.getLocalHost().getHostName();
+    } catch (UnknownHostException e) {
+      host = "Unknown";
+      e.printStackTrace();
     }
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+    // Artifact's name from the pom.xml file
+
+    model.put("build_name", buildProperties.getName());
+    model.put("build_version", buildProperties.getVersion());
+    model.put("build_time", buildProperties.getTime());
+    model.put("build_name", buildProperties.getArtifact());
+
+
+    // host
+    host += "<br>DevOps Application</br>";
+    model.put("host", host);
+    // date
+    model.put("date", sdf.format(new Date()));
+
+    return "index";
+  }
 
 }
